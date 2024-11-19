cd38d65a94cb11938760a7b39743b4015f5e63c3
“Bill” <stawdz@gmail.com>
2024-11-19 22:23:33 +0300

    srfg

--- scripts/addCar.js
+++ scripts/addCar.js
@@ -3,10 +3,12 @@ import { saveToStorage, getFromStorage } from "./utils/saveToStorage.js";
 
 const carBrandInputEl = document.querySelector('.js-addcar-input');
 const yearInputEl = document.querySelector('.js-year-input');
-yearInputEl.value = 2000;
+// yearInputEl.value = 2000; 
 const vinInputEl = document.querySelector('.js-vin-input');
 const addCarBtEl = document.querySelector('.js-add-car-bt');
+const errorMesEl = document.querySelector('.error-message-car');
 const carAddedEl = document.querySelector('.car-added-notif');
+
 
 const getCars = getFromStorage('carsData');
 let addedCarId = getCars.length;
@@ -19,26 +21,31 @@ addCarBtEl.addEventListener('click', () => {
   const carYear = yearInputEl.value;
   const carVin = vinInputEl.value;
 
-  saveToStorage('clickedCarId', addedCarId);
+  if (!carName) {
+    errorMesEl.classList.add('error-message-car--open');
+  } else {
+    errorMesEl.classList.remove('error-message-car--open');
 
-  let carData = {
-    carMaintData: [],
-    carName,
-    carYear,
-    carVin
+    let carData = {
+      carMaintData: [],
+      carName,
+      carYear,
+      carVin
+    }
+
+    carsData.push(carData);
+
+    saveToStorage('carsData', carsData);
+
+    carBrandInputEl.value = '';
+    yearInputEl.value = '';
+    vinInputEl.value = '';
+
+    carAddedEl.classList.add('car-added-notif--open');
+    setTimeout(() => (
+      carAddedEl.classList.remove('car-added-notif--open'), window.open('maintLog.html', '_self')
+    ), 1200);
+    saveToStorage('clickedCarId', addedCarId);
   }
-
-  carsData.push(carData);
-
-  saveToStorage('carsData', carsData);
-
-  carBrandInputEl.value = '';
-  yearInputEl.value = '';
-  vinInputEl.value = '';
-
-  carAddedEl.classList.add('car-added-notif--open');
-  setTimeout(() => (
-    carAddedEl.classList.remove('car-added-notif--open'), window.open('maintLog.html', '_self')
-  ), 1200);
 })
 
